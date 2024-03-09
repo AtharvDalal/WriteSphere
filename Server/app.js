857a9f0bd9ea6d363e393e4ec1f3b252d6864c38
ATHARV DALAL <sangamdalal24@gmail.com>
2024-03-09 23:44:31 +0530

    Node Js Server

--- Server/app.js
+++ Server/app.js
@@ -1,9 +1,28 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import cors from 'cors'
+import cookierParser from  "cookie-parser"
+import {dbConnection} from './DB/dbConnection.js'
+import { ErrorMiddleware } from './middlewares/error.js';
 
 
 const app  = express()
 
+app.use(cors({
+    origin:[],
+    methods:["GET","PUT","POST","DELETE"],
+    credentials:true
+}));
+
+app.use(cookierParser());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+dbConnection()
+
+app.use(ErrorMiddleware);
 
 dotenv.config({path: './config/config.env'})
+
+
 export default app;
