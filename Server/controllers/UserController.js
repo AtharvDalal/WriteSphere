5136eb7d51f9b521bfccc257d1fe4ad8ed1ad8f5
ATHARV DALAL <sangamdalal24@gmail.com>
2024-03-10 01:07:47 +0530

    Created Login Controller

--- Server/controllers/UserController.js
+++ Server/controllers/UserController.js
@@ -4,26 +4,49 @@ import { User } from "../models/userSchema.js";
 
 
 export const register = catchAsyncErrors(async (req,res,next)=>{
-     try {
-         const {name,email,phone,role,education,pasword} = req.body
-         if (!name || !email || !phone || !role || !education || !pasword) {
+    
+         const {name,email,phone,role,education,password} = req.body
+         if (!name || !email || !phone || !role || !education || !password) {
                   return next(new ErrorHandler("Please Provide Full Deatils",400))
          }
-         const user = await User.findOne({email})
-         if (user) {
+         const userexist = await User.findOne({email})
+         if (userexist) {
             return next(new ErrorHandler("User already Exist",400))
          }
          await User.create({
-           name,email,phone,role,education,pasword
+           name,email,phone,role,education,password
          });
          res.status(200).json({
            success:true,
            msg:"User Registerd SuccessFull"
          })
-     } catch (error) {
-        return res.status(400).json({
-            success:true,
-            msg:"Error In User Register API"
-        })
-     }
+     
+})
+
+export const login = catchAsyncErrors(async(req,res,next)=>{
+    const {email, password, role} = req.body
+
+    if (!email || !password || !role) {
+       return next(new ErrorHandler("Please Provide Full Credentials ",400))
+    }
+
+    const user = await User.findOne({email}).select("+password")
+    if (!user) {
+      return next(new ErrorHandler("Invalid Email or Password ",400))
+    }
+    const isPasswordMatch = await user.comparePassword(password)
+    if (!isPasswordMatch) {
+      return next(new ErrorHandler("Invalid Email or Password",400))
+    }
+    if (user.role !== role) {
+      return next(new ErrorHandler(`User Provided Role ${role} not found`,400))
+    }
+    res.status(200).json({
+      success:true,
+      msg:"User Login SuccessFull"
+    })
+
+    
+
+    
 })
