54ae7555e4b9bfc347aa29d2b06361b2ba8106d9
ATHARV DALAL <sangamdalal24@gmail.com>
2024-03-10 01:09:05 +0530

    Database Connection

--- Server/DB/dbConnection.js
+++ Server/DB/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect(process.env.MONGO_DB , {
+    .connect(process.env.MONGO_DB || "mongodb://127.0.0.1:27017" , {
       dbName: "FULL_STACK_BLOGGING_APP",
     })
     .then(() => console.log("Database Connected SuccesFully"))
